4450d10ca8c29a062aa8f31b001645d3c2f44cdc
Yuvraj Singh <yuvrajsingh091124@gmail.com>
2026-06-24 14:25:23 +0530

    Time: 2 ms (21.37%), Space: 43.7 MB (5.49%) - LeetHub

---
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countHillValley(int[] nums) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                arr.add(nums[i]);
+            }
+        }
+
+        int count = 0;
+        for (int i = 1; i < arr.size() - 1; i++) {
+            if (arr.get(i) > arr.get(i - 1) &&
+                arr.get(i) > arr.get(i + 1)) {
+                count++;
+            }
+            else if (arr.get(i) < arr.get(i - 1) &&
+                     arr.get(i) < arr.get(i + 1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
