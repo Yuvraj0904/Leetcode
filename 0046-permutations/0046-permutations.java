f21e8c4040533752d1259f871bb9565febdff665
Yuvraj Singh <yuvrajsingh091124@gmail.com>
2026-08-20 23:21:30 +0530

    Time: 3 ms (6.39%), Space: 45.5 MB (62.8%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public boolean nextPermutation(int[] nums) {
+
+        int index = -1;
+        int n = nums.length;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            return false;
+        }
+        for (int i = n - 1; i > index; i--) {
+            if (nums[i] > nums[index]) {
+                swap(nums, i, index);
+                break;
+            }
+        }
+        reverse(nums, index + 1, n - 1);
+
+        return true;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        while (true) {
+
+            List<Integer> temp = new ArrayList<>();
+
+            for (int num : nums) {
+                temp.add(num);
+            }
+
+            ans.add(temp);
+
+            if (!nextPermutation(nums)) {
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
